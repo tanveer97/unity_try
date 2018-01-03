5d5689dac4b36f7e6631f6747a859709c8939363
tanveer97 <tanveers232@gmail.com>
2018-01-03 15:47:17 +0530

    intial commit

---
+++ JUMPS.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JUMPS : MonoBehaviour {
+
+	public GameObject cube;
+	public Vector3 jumpforce;
+
+	// Use this for initialization
+	void Start () {
+		jumpforce = new Vector3 (0f, 500f, 0f);
+
+	}
+	void OnTriggerEnter(){
+		print ("haye mar dia");
+	}
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Jump")) {
+			cube.GetComponent<Rigidbody> ().AddForce (jumpforce);
+		}
+	}
+}
